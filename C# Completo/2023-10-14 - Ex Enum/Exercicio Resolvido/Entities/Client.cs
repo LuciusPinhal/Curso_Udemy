7c9cdeed923a00dd3be366eb94521c66e2651d8c
LuciusPinhal <pinhal.lucius@gmail.com>
2023-12-14 16:42:39 -0300

    :sparkles: exercicios Enum Resolvidos - Composição Objetos

---
+++ C# Completo/2023-10-14 - Ex Enum/Exercicio Resolvido/Entities/Client.cs
@@ -0,0 +1,16 @@
+﻿namespace Basic.Entities
+{
+    class Client
+    {
+        public string Name { get; set; }
+        public string email { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public Client(string name, string email, DateTime birthDate)
+        {
+            Name = name;
+            this.email = email;
+            BirthDate = birthDate;
+        }
+    }
+}
