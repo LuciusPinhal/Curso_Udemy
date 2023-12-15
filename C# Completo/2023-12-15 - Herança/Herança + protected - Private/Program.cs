592efb7da2422b89f0ebc161d8a0565f13c86af7
LuciusPinhal <pinhal.lucius@gmail.com>
2023-12-15 14:53:43 -0300

    :sparkles: Exemplos Herança

---
+++ C# Completo/2023-12-15 - Herança/Herança + protected - Private/Program.cs
@@ -0,0 +1,18 @@
+﻿using Basic.Entities;
+
+namespace Basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(8010, "Bob Brow", 100.0, 500.0);
+            Console.WriteLine(account.Balance);
+            //dessa forma fica inacessivel par que outras acessem para aterar o Balance, porem subclasses podem altera-lo
+            //account.Balance = 200;
+            account.UpdateBalance(200);
+            Console.WriteLine("Atualizado: " + account.Balance);
+
+        }
+    }
+}
