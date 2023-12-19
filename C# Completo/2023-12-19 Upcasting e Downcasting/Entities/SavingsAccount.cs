64bf764f141fb8779cba33173e5fcf5ebd5ec04a
LuciusPinhal <pinhal.lucius@gmail.com>
2023-12-19 15:11:17 -0300

    :sparkles: Add folder Upcasting e Downcasting

---
+++ C# Completo/2023-12-19 Upcasting e Downcasting/Entities/SavingsAccount.cs
@@ -0,0 +1,18 @@
+﻿namespace Basic.Entities
+{
+    class SavingsAccount : Account
+    {
+        public double InterestRage { get; set; }
+
+        public SavingsAccount() { }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRage) : base(number, holder, balance)
+        {
+            InterestRage = interestRage;
+        }
+        public void UpdateBalanceRage()
+        {
+            Balance += Balance * InterestRage;
+        }
+    }
+}
