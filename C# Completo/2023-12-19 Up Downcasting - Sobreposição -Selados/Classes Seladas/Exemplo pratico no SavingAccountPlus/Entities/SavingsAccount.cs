aeb9cfb25af78e62918d626d154008dc708a5da8
LuciusPinhal <pinhal.lucius@gmail.com>
2023-12-19 17:15:22 -0300

    :sparkles: Classes e Metodos Selados

---
+++ C# Completo/2023-12-19 Up Downcasting - Sobreposição -Selados/Classes Seladas/Exemplo pratico no SavingAccountPlus/Entities/SavingsAccount.cs
@@ -0,0 +1,27 @@
+﻿namespace Basic.Entities
+{
+    //não deixa essa classe ser herdada Sealed
+    sealed class SavingsAccount : Account
+    {
+        public double InterestRage { get; set; }
+
+        public SavingsAccount() { }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRage) : base(number, holder, balance)
+        {
+            InterestRage = interestRage;
+        }
+        public void UpdateBalanceRage()
+        {
+            Balance += Balance * InterestRage;
+        }
+
+        //Não pode ser sobrescrita novamente em outra SubClasse
+        public sealed override void Withdraw(double amount)
+        {
+            base.Withdraw(amount);
+            Balance -= 2.0;
+
+        }
+    }
+}
