08b8b0b4c756a9d9a48e73ff9dcb843aa848f0f4
LuciusPinhal <pinhal.lucius@gmail.com>
2023-12-19 15:47:53 -0300

    :sparkles: Add folder Sobreposição, palavras virtual, override e base

---
+++ C# Completo/2023-12-19 Up Downcasting - Sobreposição -Selados/Sobreposição, palavras virtual, override e base/Entities/Account.cs
@@ -0,0 +1,34 @@
+﻿
+namespace Basic.Entities
+{
+    class Account
+    {
+        public int Number { get; private set; }
+        public string Holder { get; private set; }
+        public double Balance { get; protected set; }
+
+        public Account() { }
+
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        //virtal colocando que ele pode ser sobreposto
+        public virtual void Withdraw(double amount)
+        {
+            Balance -= amount + 5.0;
+        }
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+
+        }
+        public void UpdateBalance(double newBalance)
+        {
+            Balance = newBalance;
+        }
+    }
+}
---
+++ C# Completo/2023-12-19 Up Downcasting - Sobreposição -Selados/Upcasting e Downcasting/Entities/BusinessAccount.cs
@@ -0,0 +1,23 @@
+﻿namespace Basic.Entities
+{
+    class BusinessAccount : Account
+    {
+        public double LoanLimit { get; set; }
+
+        public BusinessAccount() { }
+
+        //Não precisa declarar basta colocar o base que ele pega o construtor da SuperClasse, Account
+        public BusinessAccount(int number, string holder, double balance, double loanLimit) : base(number, holder, balance)
+        {           
+            LoanLimit = loanLimit;
+        }
+
+        public void Loan(double amount)
+        {
+            if (amount <= LoanLimit)
+            {
+                Balance += amount;
+            }
+        }
+    }
+}
