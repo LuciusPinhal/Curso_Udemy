060f8a224dcad1b3e815884c737376e1d85fe2d6
LuciusPinhal <pinhal.lucius@gmail.com>
2023-12-21 15:55:01 -0300

    :sparkles: Exercicios Polimorfismo e Herança

---
+++ C# Completo/2023-12-21 Exemplo Exercicio Polimorfismo e Herença/Exercicio Resolvido/Entities/Employee.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Basic.Entities
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public int  Hours { get; set; }
+        public double ValuePerHour { get; set; }
+
+        public Employee() { }
+        public Employee(string name, int hours, double valuePerHour)
+        {
+            Name = name;
+            Hours = hours;
+            ValuePerHour = valuePerHour;
+        }
+
+        /// <summary>
+        /// Variavel que retorna o valor do pagamento
+        /// </summary>
+        /// <returns> Hours * ValuePerHour </returns>
+        public virtual double Payment()
+        {
+            return Hours * ValuePerHour;
+        }
+
+    }
+}
