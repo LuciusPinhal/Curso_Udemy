e0779953fce4b71bb6437a941d1a1aaaa438260b
LuciusPinhal <pinhal.lucius@gmail.com>
2023-12-23 12:30:18 -0300

    :sparkles: Exercicios Resolvidos Polimorfismo e Herança

---
+++ C# Completo/2023-12-21 Exemplo Exercicio Polimorfismo e Herença/Exercicio 02/Exercicio Resolvido/Entities/ImportedProduct.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic.Entities
+{
+    class ImportedProduct : Produtc
+    {
+        public double CustoomsFee { get; set; }
+        public ImportedProduct() { }
+
+        public ImportedProduct(string name, double price, double custoomsFee) : base (name, price)
+        {
+            CustoomsFee = custoomsFee;
+        }
+        public double totalPrice()
+        {
+            return Price + CustoomsFee;
+        }
+        public override string priceTag()
+        {
+            string formatting = Name + " $ " + totalPrice().ToString("F2", CultureInfo.InvariantCulture) 
+                +" (Customs fee: $ "+ CustoomsFee.ToString("F2", CultureInfo.InvariantCulture) + ")";
+            return formatting;
+        }
+    }
+}
