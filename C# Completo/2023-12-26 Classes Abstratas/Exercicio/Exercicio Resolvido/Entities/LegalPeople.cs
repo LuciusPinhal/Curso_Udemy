2aee37c0cf97ca04190860a0b76cf64bcd5709ce
LuciusPinhal <pinhal.lucius@gmail.com>
2023-12-26 16:26:47 -0300

    :sparkles: Exercicio fixação Classes Abstratas

---
+++ C# Completo/2023-12-26 Classes Abstratas/Exercicio/Exercicio Resolvido/Entities/LegalPeople.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Basic.Entities
+{
+    class LegalPeople : People
+    {
+        public int EmployeeNumber { get; set; }
+
+        public LegalPeople(string name, double annualIncome, int employeeNumber) : base(name, annualIncome)
+        {
+            EmployeeNumber = employeeNumber;
+        }
+
+        public override double Impost()
+        {
+            double EmployeeQuantity = (EmployeeNumber <= 10) ? 0.16 : 0.14;
+
+            return AnnualIncome * EmployeeQuantity;
+        }
+    }
+}
