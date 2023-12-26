116991a863cfa20f6dc72a7cb0299c25ce4fe8f2
LuciusPinhal <pinhal.lucius@gmail.com>
2023-12-26 13:42:16 -0300

    :sparkles: Inicio Classes Abstratas

---
+++ C# Completo/2023-12-26 Classes Abstratas/Entities/SavingsAccount.cs
@@ -0,0 +1,33 @@
+﻿namespace Basic.Entities
+{
+    class SavingsAccount : Account
+    {
+        public double InterestRage { get; set; }
+
+        public SavingsAccount() { }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRage) : base(number, holder, balance)
+        {
+            InterestRage = interestRage;
+        }
+        public void UpdateBalanceRage()
+        {
+            Balance += Balance * InterestRage;
+        }
+        //dessa forma ele sobrescreve o Wthdraw
+
+        //public override void Withdraw(double amount)
+        //{
+        //    Balance -= amount;
+        //}
+
+        //dessa forma ele não sobrescreve e ainda tira os 2 reais
+        public override void Withdraw(double amount)
+        {
+            //efetua o saque de acordo com a regra da superClasse
+            base.Withdraw(amount);
+            Balance -= 2.0;
+
+        }
+    }
+}
