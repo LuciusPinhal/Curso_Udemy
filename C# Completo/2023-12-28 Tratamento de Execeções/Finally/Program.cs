05bdf35988a9bfd64de871cc58c1e226681c7ffb
LuciusPinhal <pinhal.lucius@gmail.com>
2023-12-28 15:43:26 -0300

    :sparkles: Inicio tratamento de Execoções - Finally

---
+++ C# Completo/2023-12-28 Tratamento de Execeções/Finally/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+namespace Basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = null;
+
+            try
+            {
+                fs = new FileStream(@"C:\temp\data.txt", FileMode.Open);
+                StreamReader sr = new StreamReader(fs);
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally 
+            {
+                if (fs != null)
+                {
+                    fs.Close();
+                }
+            }
+
+
+        }
+           
+    }
+}
