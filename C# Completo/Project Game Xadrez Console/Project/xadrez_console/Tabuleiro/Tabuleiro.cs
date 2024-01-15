aa4febf7aadd0f97f6407384e232ff82ae69ad5c
LuciusPinhal <pinhal.lucius@gmail.com>
2024-01-15 16:51:07 -0300

    :construction: fazendo os primeiros teste de movimentação

--- C# Completo/Project Game Xadrez Console/Project/xadrez_console/Tabuleiro/Tabuleiro.cs
+++ C# Completo/Project Game Xadrez Console/Project/xadrez_console/Tabuleiro/Tabuleiro.cs
@@ -34,6 +34,20 @@
             p.posicao = pos;
         }
 
+        public Peca retirarPeca(Posicao pos)
+        {
+            if (peca(pos) == null)
+            {
+                return null;
+            }
+
+            Peca aux = peca(pos);
+            aux.posicao = null;
+            pecas[pos.Linha, pos.Coluna] = null;
+
+            return aux;
+        }
+          
         public bool existePeca(Posicao pos)
         {
             validarPosicao(pos);
