e71c33198e903a9a51e2f96b12ce1229a668a225
LuciusPinhal <pinhal.lucius@gmail.com>
2024-01-31 11:01:02 -0300

    :construction: StreamWriter

---
+++ C# Completo/Course Codes/2024-01-30 Trabalhando com arquivos/StreamWriter/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"c:\tempo\file1.txt";
+            string targetPath = @"c:\tempo\file2.txt";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach (string line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+
+             
+            }
+            catch(IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+                
+        }
+
+    }
+}
+
+
+
