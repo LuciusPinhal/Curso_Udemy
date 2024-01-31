2cb5bde7bc68e7db85929d2987c47c2289f18def
LuciusPinhal <pinhal.lucius@gmail.com>
2024-01-31 10:45:39 -0300

    :construction: Bloco using

---
+++ C# Completo/Course Codes/2024-01-30 Trabalhando com arquivos/Bloco using/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\tempo\file1.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+             
+            }
+            catch(IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+                
+        }
+
+    }
+}
+
+
+
+
+//try
+//{
+//    using (FileStream fs = new FileStream(path, FileMode.Open))
+//    {
+//        using (StreamReader sr = new StreamReader(fs))
+//        {
+//            while (!sr.EndOfStream)
+//            {
+//                string line = sr.ReadLine();
+//                Console.WriteLine(line);
+//            }
+//        }
+//    }
+//}
+//catch (IOException ex)
+//{
+//    Console.WriteLine(ex.Message);
+//}
