2ca5734932de3c3da6a41030b4985dabd010017b
LuciusPinhal <pinhal.lucius@gmail.com>
2024-02-06 17:31:48 -0300

    :construction: exercicio de fixação

---
+++ C# Completo/Course Codes/2024-02-02 Interfaces/2024-02-06  Exercicio de fixação/Program.cs
@@ -0,0 +1,40 @@
+﻿using Basic.Entities;
+using Basic.Services;
+using System.Globalization;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter contract data");
+        Console.Write("Number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        Console.Write("Date (dd/MM/yyyy hh:mm): ");
+        DateTime date = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
+
+        Console.Write("Contract value: ");
+        double total = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        Console.Write("Enter number of installments: ");
+        int installments = int.Parse(Console.ReadLine());
+
+        Contract contract = new Contract(number, date, total);
+
+        InstallmentService installmentService = new InstallmentService(installments, new IPayPalTaxService());
+
+        Console.WriteLine();
+        Console.WriteLine("Installments: ");
+        Console.WriteLine();
+
+        installmentService.PaymentInstallmentValue(contract);
+
+        foreach (Installment installment in contract.installment)
+        {
+            Console.WriteLine(installment);
+        }
+
+    }
+}
+
+
---
+++ C# Completo/Course Codes/2024-02-02 Interfaces/2024-02-06  Exercicio de fixação/Services/IPaymentService.cs
@@ -0,0 +1,8 @@
+﻿namespace Basic.Services
+{
+    interface IPaymentService
+    {
+        double Tax(double amount);
+        double Interest(double amount);
+    }
+}
